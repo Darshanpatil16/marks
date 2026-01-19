9434cc6d2c0946d4e15818dbcd4390e07506a210
Darshanpatil16 <darshan16062006@gmail.com>
2026-01-19 11:58:25 +0530

    Update marks.py

--- marks.py
+++ marks.py
@@ -1,22 +1,19 @@
 import sys
-import math
+
 def avg_marks(marks):
-    
-    avg=sum(marks)/len(marks)
+    avg = sum(marks) / len(marks)
     return avg
-if  __name__ == "__main__":
-    if len(sys.argv)>1:
-        marks=[
-            a=float(sys.argv[1])
-            b=float(sys.argv[2])
-           c= float(sys.argv[3])
-        ]
-        
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        marks = list(map(int, sys.argv[1:]))
     else:
-        marks=[90,80,98,76,69]
-        result=avg_marks(marks)
-        print("Average Marks is:",result)
-        if result>=80:
-            print("A")
-        else:
-            print("B")
+        marks = [90, 80, 98, 76, 69]
+
+    result = avg_marks(marks)
+    print("Average Marks is:", result)
+
+    if result >= 80:
+        print("Grade: A")
+    else:
+        print("Grade: B")
