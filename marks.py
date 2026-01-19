2b97c8b494e02b6ce2a5c2747394f66422a407f2
Darshanpatil16 <darshan16062006@gmail.com>
2026-01-19 11:28:56 +0530

    save

---
+++ marks.py
@@ -0,0 +1,17 @@
+import sys
+import math
+def avg_marks(marks):
+    
+    avg=sum(marks)/len(marks)
+    return avg
+if  __name__ == "__main__":
+    if len(sys.argv)>1:
+        marks=list(map(int,sys.argv[1:]))
+    else:
+        marks=[90,80,98,76,69]
+        result=avg_marks(marks)
+        print("Average Marks is:",result)
+        if result>=80:
+            print("A")
+        else:
+            print("B")
