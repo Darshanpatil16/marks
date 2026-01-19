5d80eb0dee7d351641bc8c020c254d205691aff0
Darshanpatil16 <darshan16062006@gmail.com>
2026-01-19 11:47:51 +0530

    Update marks.py

--- marks.py
+++ marks.py
@@ -6,7 +6,8 @@ def avg_marks(marks):
     return avg
 if  __name__ == "__main__":
     if len(sys.argv)>1:
-        marks=list(map(int,sys.argv[1:]))
+        marks[]=list(map(int,sys.argv[1:]))
+        
     else:
         marks=[90,80,98,76,69]
         result=avg_marks(marks)
